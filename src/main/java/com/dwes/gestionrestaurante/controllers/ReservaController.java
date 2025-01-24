ccb679e4692a70ed4b69c80211da99763ec2ef7a
dawcarlos <dawcarlospereira@gmail.com>
2025-01-24 18:33:22 +0100

    Eliminar reserva

--- src/main/java/com/dwes/gestionrestaurante/controllers/ReservaController.java
+++ src/main/java/com/dwes/gestionrestaurante/controllers/ReservaController.java
@@ -4,6 +4,9 @@ import com.dwes.gestionrestaurante.repositories.ClienteRepository;
 import com.dwes.gestionrestaurante.repositories.MesaRepository;
 import com.dwes.gestionrestaurante.repositories.ReservaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,4 +19,14 @@ public class ReservaController {
     private ClienteRepository clienteRepository;
     @Autowired
     private MesaRepository mesaRepository;
+    /* Eliminar reserva */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteReserva(@PathVariable Long id) {
+        return mesaRepository.findById(id)
+                .map(reserva -> {
+                    mesaRepository.delete(reserva);
+                    return ResponseEntity.noContent().build(); // HTTP 204 No Content
+                })
+                .orElse(ResponseEntity.notFound().build()); // HTTP 404 Not Found
+    }
 }
