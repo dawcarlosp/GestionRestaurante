d9e06b45ae4c16cf9d85418cd8b8e413bdad1b2a
dawcarlos <dawcarlospereira@gmail.com>
2025-01-24 16:32:35 +0100

    Primer commit

---
+++ src/test/java/com/dwes/gestionrestaurante/GestionRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dwes.gestionrestaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionRestauranteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
