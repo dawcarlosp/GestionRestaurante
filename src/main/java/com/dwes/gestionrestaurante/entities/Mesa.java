d8eb0e8753f30527b5317ce41e5c4d212db7efee
CarlosP <dawcarlospereira@gmail.com>
2025-01-30 14:24:43 +0100

    Mejora de la validacion de Mesa a la hora de crear

--- src/main/java/com/dwes/gestionrestaurante/entities/Mesa.java
+++ src/main/java/com/dwes/gestionrestaurante/entities/Mesa.java
@@ -2,6 +2,8 @@ package com.dwes.gestionrestaurante.entities;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.*;
 
 import java.util.List;
@@ -16,7 +18,8 @@ public class Mesa {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    @NotBlank(message = "{mesa.numeroMesa.notBlank}")
+    @NotNull(message = "{mesa.numeroMesa.notNull}")
+    @Positive(message = "{mesa.numeroMesa.positive}")
     private int numeroMesa;
     private String Descripcion;
     @OneToMany(mappedBy = "mesa", cascade = CascadeType.ALL)
