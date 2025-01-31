1045884d3f1c37961c9d44d4c7741c223ff2bda3
Carlos P <dawcarlospereira@gmail.com>
2025-01-31 18:42:53 +0100

    UserDetailsServiceImpl ha sido creado, aun queda pulir

---
+++ src/main/java/com/dwes/gestionrestaurante/services/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.dwes.gestionrestaurante.services;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private UserEntityRepository userRepository;
+
+    UserDetailsServiceImpl(UserEntityRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        //Extrae el usuario de la BD
+        return this.userRepository.findByUsername(username).orElseThrow(
+                ()-> new UsernameNotFoundException(username+" no encontrado")
+        );
+
+    }
+}
